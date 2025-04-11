0e965b1754708e9ffc81a69f991a9654c5b1a9c1
Cabrel10 <cabrelkaka@gmail.com>
2025-04-11 18:07:53 +0200

    colab solution1

--- Morningstar/__init__.py
+++ Morningstar/__init__.py
@@ -1,11 +1,13 @@
 """Morningstar package - Crypto trading toolkit"""
-from importlib.metadata import version
+# Remplacer l'import de importlib.metadata par l'import depuis version.py
+# from importlib.metadata import version 
+from ..version import __version__
 from Morningstar.configs.logging_config import setup_logging
 
 # Initialisation des logs
 setup_logging()
 
-__version__ = version("Morningstar")
+# La ligne __version__ = version("Morningstar") est supprimée car __version__ est importé
 
 # Import des modules principaux
 from .model.architecture import morningstar_model
